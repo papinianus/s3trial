bd2e6999b7ff616c64126372bba4f7f460657c2d
papinianus <dglmsksooah@gmail.com>
2020-11-24 10:08:43 +0900

    get via presigned url

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 // Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
 // Set the region 
 AWS.config.update({region: 'ap-northeast-1'});
 
 // Create S3 service object
-s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-// Call S3 to list the buckets
-s3.listBuckets(function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data.Buckets);
-  }
-});
+const params = {Bucket: 'sample', Key: 'test.jpg', Expires: 60};
+
+var url = s3.getSignedUrl('getObject', params);
+console.log('The URL is', url); // expires in 60 seconds
